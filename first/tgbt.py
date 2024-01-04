3f12684ea75c42b574c3c5a92a3030ae0aaf0001
acer <nilz00049@gmail.com>
2024-01-05 02:30:52 +0400

    эхо бот попытка 1 с хендлерами в нем

--- first/tgbt.py
+++ first/tgbt.py
@@ -1,32 +1,24 @@
-import requests
-import time
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 
-api_url = 'https://api.telegram.org/bot'
-cat_api_url = 'https://random.dog/woof.json'
 bot_token = '6742804978:AAESNA7-XrAn6dXK64tzGE3tt5EtJzxkzRY'
-er_text = 'тут должна быть картинка с псиной, но у разраба руки из жопы'
-mxk = 100
+bot = Bot(token = bot_token)
+dp = Dispatcher()
 
-offset = -2
-counter = 0
-cat_resp: requests.Response
-cat_link: str
-chat_id: int
+@dp.message(Command(commands = 'start'))
+async def process_start_command(message: Message):
+    await message.answer('Привет! я бот-попугай')
+@dp.message(Command(commands = 'help'))
+async def help_message(message: Message):
+    await message.answer(
+        'напиши мне что-то'
+        'а я за тобой повторю. аххаа как оригинально'
+    )
 
-while counter < mxk:
-    print('attemp =', counter)
-    updates = requests.get(f'{api_url}{bot_token}/getUpdates?offset={offset+1}').json()
+@dp.message()
+async def messg(message: Message):
+    await message.answer(text = message.text)
 
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat_resp = requests.get(cat_api_url)
-            if cat_resp == 200:
-                cat_link = cat_resp.json()[0]['url']
-                requests.get(f'{api_url}{bot_token}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else: requests.get((f'{api_url}{bot_token}/sendMessage?chat_id={chat_id}&text={er_text}'))
-
-
-    time.sleep(1)
-    counter+=1
+if __name__ == '__main__':
+    dp.run_polling(bot)
